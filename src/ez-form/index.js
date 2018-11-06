7f03c3a7079e436f9c59e7d999a2089d9b0b5022
Milos Dzepina <milos.dzepina@ezderm.com>
2018-11-06 16:14:59 +0100

    Add .gitignore

--- src/ez-form/index.js
+++ src/ez-form/index.js
@@ -1,8 +1,37 @@
-import React, { useState } from "react";
+// @flow
+import React from "react";
+import type { ComponentType } from "react";
+
+type IntlMessageDescriptor = {
+  id: string,
+  defaultMessage: string,
+  description: ?string
+};
+
+type IntlMessage = {
+  descriptor: IntlMessageDescriptor,
+  values: ?{}
+};
+
+type FormElement = {
+  type: string,
+  Component: ComponentType<{ value: any, error: string, onChange: any => void }>
+};
+
+type FieldMetadata = {
+  defaultValue: string | number | boolean | Array<string>,
+  formElement: FormElement,
+  label: string | IntlMessage,
+  label2: string | IntlMessage
+};
+
+type Schema = { [string]: FieldMetadata };
 
 export const InputTypes = {
   TEXT: "TEXT_INPUT",
   SELECT: "SELECT_INPUT"
 };
 
-export default function useForm(schema) {}
+export default function useForm(schema) {
+  return {}
+}
