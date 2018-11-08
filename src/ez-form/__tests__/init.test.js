915e20ecae40fddada061df0cfd95ea6f429f1ac
Emilija <emilija.tucovic@ezderm.com>
2018-11-08 14:51:08 +0100

    Add test for second arg in useForm function

--- src/ez-form/__tests__/init.test.js
+++ src/ez-form/__tests__/init.test.js
@@ -105,3 +105,76 @@ describe("init default value from schema", () => {
     expect(TextInputComponent3).not.toBeTruthy();
   });
 });
+
+describe("Init value in schema with second arg in useForm", () => {
+  test.skip("Text input", () => {
+    function TestForm(props) {
+      const formData = useForm(
+        {
+          testInputText1: {
+            formElement: formElements.textInput,
+            defaultValue: "testInputText1",
+            name: "testInputText1"
+          },
+          testInputText2: {
+            formElement: formElements.textInput,
+            name: "testInputText2"
+          }
+        },
+        { testInputText1: "initText1", testInputText2: "initText2" }
+      );
+
+      return (
+        <div>
+          {formData.testInputText1.render()}
+          {formData.testInputText2.render()}
+        </div>
+      );
+    }
+
+    const { container } = render(<TestForm />);
+
+    const inputs = container.querySelectorAll("input");
+
+    expect(inputs[0].value).toBe("initText1");
+    expect(inputs[1].value).toBe("initText2");
+  });
+
+  test.skip("Select", () => {
+    function TestForm(props) {
+      const formData = useForm(
+        {
+          testSelectStringValue: {
+            formElement: formElements.select
+          },
+          testSelectObjectValue: {
+            formElement: formElements.select
+          }
+        },
+        {
+          testSelectStringValue: "test-select",
+          testSelectObjectValue: {
+            value: "test-select",
+            label: "Test Select Init 2"
+          }
+        }
+      );
+
+      return (
+        <div>
+          {formData.testSelectStringValue.render({
+            options: [{ value: "test-select", label: "Test Select Init 1" }]
+          })}
+          {formData.testSelectObjectValue.render({
+            options: [{ value: "test-select", label: "Test Select Init 2" }]
+          })}
+        </div>
+      );
+    }
+
+    const { queryByText } = render(<TestForm />);
+
+    expect(queryByText("Test Select Init 1")).toBeTruthy();
+    expect(queryByText("Test Select Init 2")).toBeTruthy();
+  });
+})
